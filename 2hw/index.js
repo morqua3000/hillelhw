468ab6831993a4af3a56aecb7f1d85b283d0f4c0
morqua3000 <111141923+morqua3000@users.noreply.github.com>
2022-11-29 02:27:40 +0200

    Add files via upload

---
+++ 2hw/index.js
@@ -0,0 +1,7 @@
+let a=prompt("Insert first digit", "");
+let b=prompt("Insert second digit", "");
+const plusValue = +a + +b;
+const minusValue = a - b;
+const multiValue = a * b;
+const divideValue = a / b;
+alert(a + "+" + b + "=" + plusValue + "\n" + a + "-" + b + "=" + minusValue + "\n" + a + "*" + b + "=" + multiValue + "\n" + a + "/" + b + "=" + divideValue);
