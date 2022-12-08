12a634e9d6eb5ba064086e59d95fd77fa14fb389
morqua3000 <111141923+morqua3000@users.noreply.github.com>
2022-12-08 23:58:38 +0200

    Add files via upload

---
+++ 5hw/index.js
@@ -0,0 +1,7 @@
+let row=prompt("Enter the first row", "");
+let row2=prompt("Enter the second row", "");
+let row3=prompt("Enter the third row", "");
+alert (row.concat(row2, row3));
+
+let [firstDigit, secondDigit, thirdDigit, fourthDigit, fifthDigit] = prompt("Enter a five digit number", "".split(''));
+alert (firstDigit + " " + secondDigit + " " + thirdDigit + " " + fourthDigit + " " + fifthDigit);
