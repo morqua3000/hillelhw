9243a9a1d4e95194740d934ac82c813d6320f47d
morqua3000 <111141923+morqua3000@users.noreply.github.com>
2022-12-13 16:36:10 +0200

    Add files via upload

---
+++ 6hw/index.js
@@ -0,0 +1,5 @@
+let digit1 = prompt("Enter first number", "");
+let digit2 = prompt("Enter second number", "")
+let digit3 = prompt("Enter third number", "");
+const value = (+digit1 + +digit2 + +digit3) / 3; 
+alert ("Arithmetic mean is " + value);
